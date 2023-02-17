f0cb69acda146d71e71ddff7735fcf407bbe51da
Onkar Jadhav <omjadhav2610gmail.com>
2023-02-17 17:26:11 +0530

    Add @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MediaModule } from './media/media.module';
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [MediaModule],
+  imports: [MediaModule, ConfigModule.forRoot({isGlobal: true})],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
